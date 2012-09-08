b4dbdcd422dab59709bebfc3611e337cced5d101
ossreleasefeed <sneethling@mozilla.com>
2012-09-08 16:55:49 +0200

    updated README

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 Sandstone
 =========
 
-A bootstrap for static sites in the Mozilla Universe
+A bootstrap for static sites in the Mozilla Universe. This project aims to automate as much
+of the process of starting a new website using the Mozilla Sandstone theme as possible and,
+also provides the tools, via Grunt, to preprocess, lint and minify/uglify the assets.
+
+Prerequisites
+=============
+
+You will currently need to follow prerequisites installed:
+
+* [Nodejs][nodejs]
+* [Volo][volo]
+* [lessc][lessc]
+
+[nodejs]: http://nodejs.org
+[volo]: https://github.com/volojs/volo
+[lessc (Look under the Server-Side install heading)]: hhttp://lesscss.org/#-client-side-usage
--- grunt.js
+++ grunt.js
@@ -3,16 +3,16 @@ module.exports = function(grunt) {
     grunt.initConfig({
         less: {
             sandstone: {
-                src: 'media/css/sandstone/sandstone.less',
-                dest: 'media/css/sandstone.css'
+                src: 'css/sandstone/sandstone.less',
+                dest: 'css/sandstone.css'
             },
             sandstone_resp: {
-                src: 'media/css/sandstone/sandstone-resp.less',
-                dest: 'media/css/sandstone-resp.css'
+                src: 'css/sandstone/sandstone-resp.less',
+                dest: 'css/sandstone-resp.css'
             }
         },
         lint: {
-            files: ['grunt.js', 'media/js/*.js']
+            files: ['grunt.js', 'js/*.js']
         }
     });
 
